459954dc0dc329c658712a27d0acb78a42e4042d
NiteNova <112674365+NiteNova@users.noreply.github.com>
2023-01-09 08:42:57 -0700

    Create DC 1.9 (For Loops).cpp

---
+++ DC 1.9 (For Loops).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 70; i <= 80; i += 1)
+        cout << i << " ";
+
+    cout << endl;
+
+    for (int g = 100; g <= 300; g += 20)
+        cout << g << " ";
+
+    cout << endl;
+
+    for (int t = 25; t >= -25; t -= 5)
+        cout << t << " ";
+
+    cout << endl;
+
+    for (int P = 10000; P >= 9; P /= 5)
+        cout << P << " ";
+
+}
