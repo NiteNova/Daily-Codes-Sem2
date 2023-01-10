5e14d528ef4084185021464cf347d7336ea2f1a5
NiteNova <112674365+NiteNova@users.noreply.github.com>
2023-01-10 09:26:55 -0700

    Create DC 1.10 (Nested For loops).cpp

---
+++ DC 1.10 (Nested For loops).cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << "*";
+
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << "*";
+
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 9; j++) {
+			cout << "*";
+
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 9; j++) {
+			cout << i ;
+
+
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 1; j++) {
+			cout << "012";
+
+
+		}
+		cout << endl;
+	}
+} 
