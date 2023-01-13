466b50ea6c284264b2867be4a7a1f799af9e7ba7
NiteNova <112674365+NiteNova@users.noreply.github.com>
2023-01-13 08:24:11 -0700

    Create 1.13 Assessment (For loops).cpp

---
+++ 1.13 Assessment (For loops).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 50; i <= 70; i += 2)
+		cout << i << " ";
+
+	cout << endl;
+	cout << endl;
+
+	for (int g = 100; g >= 50; g -= 5)
+		cout << g << " ";
+
+	cout << endl;
+	cout << endl;
+
+	for (int g = 2; g <= 2048; g *= 2)
+		cout << g << " ";
+
+	cout << endl;
+	cout << endl;
+}
