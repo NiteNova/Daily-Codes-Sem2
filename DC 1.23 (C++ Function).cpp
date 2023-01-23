b689d8bfe5fca51c57622aea1f83e62303de0f14
NiteNova <112674365+NiteNova@users.noreply.github.com>
2023-01-23 08:50:57 -0700

    Create DC 1.23 (C++ Function).cpp

---
+++ DC 1.23 (C++ Function).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//function declaration
+void helpmenu();
+char userinput;
+
+int main() {
+
+	helpmenu();
+
+
+}
+
+//function definition
+void helpmenu() {
+	cout << "Enter i for inventory" << endl;
+	cout << "Enter c for command options" << endl;
+	cout << "Enter m for your map" << endl;
+	cout << "Enter q to quit" << endl;
+	
+}
