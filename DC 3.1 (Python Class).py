c171df86c079b377c01c172a546f615a479a71ff
NiteNova <112674365+NiteNova@users.noreply.github.com>
2023-03-01 09:43:05 -0700

    Update DC 3.1 (Python Class).py

--- DC 3.1 (Python Class).py
+++ DC 3.1 (Python Class).py
@@ -5,7 +5,7 @@ class chicken:
         self.name = name
         self.hunger = 0
 
-    def update(self, hunger):
+    def update(self):
         self.hunger += 5
         if self.hunger < 30:
            i = random.randrange(1, 3)
@@ -22,7 +22,7 @@ class chicken:
     def pet(self):
         print("bawk bawk")
        
-    def printinfo(self, name):
+    def printinfo(self):
         print(self.name)
         print("Chicken:", self.name,"hunger's ", self.hunger)
 
@@ -35,20 +35,20 @@ c3 = chicken("Alan")
 
 gameloop = True
 while gameloop == True:
-    numEggs += c1.update("Kevin")
-    numEggs += c2.update("Alex")
-    numEggs += c3.update("Alan")
+    numEggs += c1.update()
+    numEggs += c2.update()
+    numEggs += c3.update()
     print(numEggs)
     choice = input("What would you like to do with your chicken?: Pet, Feed, Printinfo: ")
     if choice == "Pet" or choice == 'pet':
-        c1.pet("Kevin")
-        c2.pet("Alex")
-        c3.pet("Alan")
+        c1.pet()
+        c2.pet()
+        c3.pet()
     elif choice == "Feed":
         c1.feed(5)
         c2.feed(10)
         c3.feed(6)
     elif choice == "Printinfo":
-        c1.printinfo("Kevin")
-        c2.printinfo("Alex")
-        c3.printinfo("Alan")
+        c1.printinfo()
+        c2.printinfo()
+        c3.printinfo()
